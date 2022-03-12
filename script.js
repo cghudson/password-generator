4573bf172b66499e407fa8a5a388d9528a3adb1d
Carolyn <cghudson@uwalumni.com>
2022-03-12 17:22:15 -0600

    write pseudo code

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// click button to generate password
+// user is propted for password criteria
+// propt to choose length of password (8 - 128 characters)
+// prompts for character types **must select at least one
+  //lowercase 
+  //uppercase
+  //numeric
+  //special characters
+// password is generated once all propts are answered
+// generated password is displayed in alert or written on page
