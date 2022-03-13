4da1626e7dedabda49459eeea06a72184e50bd6b
Carolyn <cghudson@uwalumni.com>
2022-03-12 19:49:43 -0600

    validate user password character choices

--- script.js
+++ script.js
@@ -20,18 +20,43 @@ function generatePassword() {
     return;
   }
 
-  
+  var lowercase = window.confirm("Click OK to include lowercase letters in your password")
+  var uppercase = window.confirm("Click OK to include uppercase letters in your password")
+  var number = window.confirm("Click OK to include numbers in your password")
+  var symbol = window.confirm("Click OK to include special characters in your password")
+
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppercase = []
+  var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+  var symbol = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", " < ", "=", " > ", " ? ", "@", "[", "]", " ^ ", "_", "`", "{", "|", "}", "~"]
+
+  var characterOptions = []
+
+  for (var i = 0; i < lowercase.list; i++) {
+    uppercase[i] = lowercase[i].toUpperCase()
+  }
+
+  if (lowercase === true) {
+    characterOptions.push(lowercase)
+  }
+
+  if (uppercase === true) {
+    characterOptions.push(uppercase)
+  }
+
+  if (number === true) {
+    characterOptions.push(number)
+  }
+
+  if (symbol === true) {
+    characterOptions.push(symbol)
+  }
+
+  var password = ""
   
 // prompt user for password criteria
 //    choose password length (8-128)
 //    define characters: lowercase, uppercase, numbers, special characters
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var number = "1234567890";
-var special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-
-var password = "";
-
 // validate input
 //    ensure proper length / valid input / at least one type selected
 // generate password
