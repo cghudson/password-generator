bd34864a3dfd2e6fd962a39e8dc47166eafb6942
Carolyn <cghudson@uwalumni.com>
2022-03-12 19:20:21 -0600

    password length and number alerts

--- script.js
+++ script.js
@@ -5,6 +5,22 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+
+  var userInput = window.prompt("How many characters would you like your password to be?")
+
+  var passwordLength = parseInt(userInput)
+
+  if (isNaN(passwordLength)) {
+    window.alert("Please input a number!")
+    return;
+  }
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password length must be between 8 and 128 characters")
+    return;
+  }
+
+  
   
 // prompt user for password criteria
 //    choose password length (8-128)
@@ -12,7 +28,9 @@ function generatePassword() {
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "1234567890";
-var specialChar = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+var password = "";
 
 // validate input
 //    ensure proper length / valid input / at least one type selected
