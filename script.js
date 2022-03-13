2d25035c74c074bd56193508f9764ae55df84b4a
Carolyn <cghudson@uwalumni.com>
2022-03-12 18:10:44 -0600

    character variables, define generate password function

--- script.js
+++ script.js
@@ -4,26 +4,32 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  
+// prompt user for password criteria
+//    choose password length (8-128)
+//    define characters: lowercase, uppercase, numbers, special characters
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number = "1234567890";
+var specialChar = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+// validate input
+//    ensure proper length / valid input / at least one type selected
+// generate password
+
+// display generated password
+  return "generated password";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password; // password displayed on screen
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// click button to generate password
-// user is propted for password criteria
-// propt to choose length of password (8 - 128 characters)
-// prompts for character types **must select at least one
-  //lowercase 
-  //uppercase
-  //numeric
-  //special characters
-// password is generated once all propts are answered
-// generated password is displayed in alert or written on page
+generateBtn.addEventListener("click", writePassword);  // when user clicks button, will call write password function
